23ff25b8060eabbb2356bf452afe7262855bc19a
maxdemo1 <maxbrownwot@gmail.com>
2023-09-16 22:55:02 +0300

    update v0.0.7.2

--- js/script.js
+++ js/script.js
@@ -5,6 +5,26 @@ const second_list = document.getElementById('employee-second-list');
 const selectleft = document.getElementById('select-left');
 const selectright = document.getElementById('select-right');
 
+const checkbox2 = document.getElementById('productsbutton');
+const first_list_products = document.getElementById('products-list');
+const second_list_products = document.getElementById('products-list-part-two');
+const selectleft_products = document.getElementById('select-left-products');
+const selectright_products = document.getElementById('select-right-products');
+
+
+checkbox2.addEventListener('change', function () {
+  if (checkbox2.checked) {
+      first_list_products.style.transform = 'translate(0, 0)';
+      second_list_products.style.transform = 'translate(0, 0)';
+      selectright_products.style.opacity = '0';
+      selectleft_products.style.opacity = '1';
+  } else {
+      first_list_products.style.transform = 'translatex(-100%)';
+      second_list_products.style.transform = 'translatex(-100%)';
+      selectright_products.style.opacity = '1';
+      selectleft_products.style.opacity = '0';
+  }
+});
 
 checkbox1.addEventListener('change', function () {
   if (checkbox1.checked) {
